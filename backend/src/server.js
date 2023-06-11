c5f04ee8bfe430479601a4ecc56b31430a957a19
Jean F. dos Santos <jeanddg@hotmail.com>
2023-06-11 15:17:36 -0300

    fix: correct  message on bootstrap app

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,4 +2,4 @@ import { app } from './app.js';
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => console.log('Running server on port PORT PORT ' + PORT));
+app.listen(PORT, () => console.log('Running server on port ' + PORT));
