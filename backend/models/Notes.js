19c39b9101d56ca3f77beaf11da9e5d522a2586b
Jean F. dos Santos <jeanddg@hotmail.com>
2023-06-11 15:16:19 -0300

    feat: Note model

---
+++ backend/models/Notes.js
@@ -0,0 +1,40 @@
+import mongoose, { Schema } from 'mongoose';
+import mongoSequence from 'mongoose-sequence';
+
+const AutoIncrement = mongoSequence(mongoose);
+
+const noteSchema = new Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      default: 'Employee',
+    },
+
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+noteSchema.plugin(AutoIncrement, {
+  inc_field: 'ticket',
+  id: 'ticketNums',
+  start_seq: 500,
+});
+
+const Note = mongoose.model('Note', noteSchema);
+
+export { User };
