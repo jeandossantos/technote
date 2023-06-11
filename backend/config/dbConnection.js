c78b948aae6fdb85d1b9faf89f741a35125f76fb
Jean F. dos Santos <jeanddg@hotmail.com>
2023-06-11 15:15:53 -0300

    feat: mongoose connect

---
+++ backend/config/dbConnection.js
@@ -0,0 +1,15 @@
+import { config } from 'dotenv';
+import mongoose from 'mongoose';
+
+config();
+
+async function connectDb() {
+  try {
+    await mongoose.connect(process.env.DATABASE_URI);
+    console.log('Connected to mongodb successfully.');
+  } catch (error) {
+    console.log('🚀 ~ file: dbConnection.js:11 ~ connectDb ~ error:\n', error);
+  }
+}
+
+export { connectDb };
