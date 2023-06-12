c61af919787a4ab30678afa3c1835496c72ea66e
Jean F. dos Santos <jeanddg@hotmail.com>
2023-06-12 19:16:48 -0300

    redux

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,17 @@ import { Route, BrowserRouter, Routes } from 'react-router-dom';
 
 import App from './App.jsx';
 import './index.css';
+import { Provider } from 'react-redux';
+import { store } from './app/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path={'/*'} element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path={'/*'} element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
